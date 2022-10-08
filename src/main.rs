24cd4dac6ee7802771690f7dbdcff1557fdc149e
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-10-08 23:13:59 +0900

    add: observerパターンを追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod observer;
+
+use observer::{ConcreteObserver, ConcreteSubject, EventObject, Subject};
 fn main() {
     println!("Hello, world!");
+    observer();
+}
+
+fn observer() {
+    let mut subject = ConcreteSubject::new();
+    subject.register_observer(Box::new(ConcreteObserver(1)));
+    subject.register_observer(Box::new(ConcreteObserver(2)));
+    subject.register_observer(Box::new(ConcreteObserver(3)));
+
+    subject.notify_observers(&EventObject(100));
+    subject.notify_observers(&EventObject(200));
 }
---
+++ src/observer.rs
@@ -0,0 +1,58 @@
+///#Observerパターン
+///
+/// ### つまづいたところ
+/// - トレイトオブジェクトにdynをつけるのを忘れずに
+/// - タプル構造体のフィールドにも、pubが必要
+/// - トレイト関数の引数には、型名だけでなく、変数名も必要
+
+pub trait Subject<T: Clone> {
+    fn register_observer(&mut self, observer: Box<dyn Observer<T>>) -> usize;
+    fn remove_observer(&mut self, id: usize) -> Option<Box<dyn Observer<T>>>;
+    fn notify_observers(&self, event: &T);
+}
+
+pub trait Observer<T: Clone> {
+    fn update(&self, observer: &T);
+}
+
+#[derive(Debug, Clone)]
+pub struct EventObject(pub usize);
+
+pub struct ConcreteSubject {
+    observers: Vec<Option<Box<dyn Observer<EventObject>>>>,
+}
+
+impl ConcreteSubject {
+    pub fn new() -> ConcreteSubject {
+        ConcreteSubject {
+            observers: Vec::new(),
+        }
+    }
+}
+
+impl Subject<EventObject> for ConcreteSubject {
+    fn notify_observers(&self, e: &EventObject) {
+        for observer in self.observers.iter() {
+            match *observer {
+                Some(ref observer) => observer.update(&e),
+                None => (),
+            }
+        }
+    }
+
+    fn register_observer(&mut self, observer: Box<dyn Observer<EventObject>>) -> usize {
+        self.observers.push(Some(observer));
+        self.observers.len() - 1
+    }
+
+    fn remove_observer(&mut self, index: usize) -> Option<Box<dyn Observer<EventObject>>> {
+        self.observers[index].take()
+    }
+}
+
+pub struct ConcreteObserver(pub usize);
+impl Observer<EventObject> for ConcreteObserver {
+    fn update(&self, e: &EventObject) {
+        println!("Observer {} received event {:?}", self.0, e);
+    }
+}
